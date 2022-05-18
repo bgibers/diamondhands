961755998bbf11f23333e2926f66f623c2dcb706
Brendan Giberson <bgibers@clemson.edu>
2022-05-18 16:26:09 -0400

    Tweak diamond factory

--- diamondFactory/index.js
+++ diamondFactory/index.js
@@ -2,39 +2,67 @@ const trianglify = require('trianglify')
 var colors = require('nice-color-palettes');
 const fs = require('fs')
 
-var metadata = {
-  "Chroma type" : "shadow",
-  "Diamond size" : "25",
-  "Diamond variance" : "1",
-  "Number of colors" : "2"
+for(var i = 0; i < 1000; i ++) {
+
+    var colorFunction;
+    var metadata = {
+      "Chroma type" : "shadow",
+      "Diamond size" : "25",
+      "Diamond variance" : "1",
+      "Color1" : "2",
+      "Color2" : "2",
+      "Color3" : "2",
+      "Color4" : "2",
+      "Color5" : "2"
+    }
+
+    if (randomInteger(1,2) % 2) {
+      colorFunction = trianglify.colorFunctions.shadows(getRandom(.2,1));
+      metadata['Chroma type'] = "shadow";
+    } else {
+      colorFunction = trianglify.colorFunctions.sparkle(getRandom(.2,1));
+      metadata['Chroma type'] = "sparkle";
+    }
+
+    var cellSize = getRandom(25,50);
+    metadata['Diamond size'] = cellSize;
+
+    var cellVar = getRandom(.6, 1);
+    metadata['Diamond variance'] = cellVar;
+
+    var color = [colors[randomInteger(0,99)]]
+    metadata.Color1 = color[0][0],
+    metadata.Color2 = color[0][1],
+    metadata.Color3 = color[0][2],
+    metadata.Color4 = color[0][3],
+    metadata.Color5 = color[0][4]
+
+    const defaultOptions = {
+        width: 1500,
+        height: 1500,
+        cellSize: cellSize,//25-50
+        variance: cellVar, // random bw 0-1
+        seed: null,
+        xColors: 'random',
+        yColors: 'random',
+        fill: true,
+        colorSpace: 'lab',
+        palette: color,
+        colorFunction: colorFunction, // sparkle and shadow .2 - 1 random
+        strokeWidth: 0
+    }
+    
+    const canvas = trianglify(defaultOptions).toCanvas()
+    const file = fs.createWriteStream(`../ipfs/assets/backgrounds/trianglify${i}.png`)
+    canvas.createPNGStream().pipe(file)
+
+    fs.writeFile(`../ipfs/assets/metadata/trianglify${i}.json`, JSON.stringify(metadata), () => {})
 }
-
-fs.writeFile(test.json, metadata)
-
-// for(var i = 0; i < 1000; i ++) {
-
-//     var colorFunction = getRandom(1,1000) % 2 ? trianglify.colorFunctions.shadows(getRandom(.2,1)) : trianglify.colorFunctions.sparkle(getRandom(.2,1));
-    
-//     const defaultOptions = {
-//         width: 1500,
-//         height: 1500,
-//         cellSize: getRandom(25,50),//25-50
-//         variance: 1, // random bw 0-1
-//         seed: null,
-//         xColors: 'random',
-//         yColors: 'random',
-//         fill: true,
-//         colorSpace: 'lab',
-//         palette: colors,
-//         colorFunction: colorFunction, // sparkle and shadow .2 - 1 random
-//         strokeWidth: 0
-//     }
-    
-//     const canvas = trianglify(defaultOptions).toCanvas()
-//     const file = fs.createWriteStream(`../ipfs/assets/backgrounds/trianglify${i}.png`)
-//     canvas.createPNGStream().pipe(file)
-// }
 
 function getRandom(min, max) {
     return Math.random() * (max - min) + min;
-  }
+}
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
